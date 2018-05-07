24b3930525f4458f22f94788b6babaa01f8c2290
Drew Graham <Drew Graham>
2018-05-07 19:30:43 -0400

    /game request takes a game name now (not sure how i'll structure serving game content yet)

--- server.js
+++ server.js
@@ -10,5 +10,12 @@ app.listen(8080, function(){
 });
 
 app.get('/game', function(req, res){
-    res.sendFile(path.join(__dirname + "/game.html"));
+	res.sendFile(path.join(__dirname + "/game.html"));
+	console.log(req.query.name);
+	//getDescription("gameName");
 });
+
+
+function getDescription(game) {
+	console.log(game);
+}
